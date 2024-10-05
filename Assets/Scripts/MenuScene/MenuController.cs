9eccc71abf60736c9952ea92fe07fd84335e3ad5
longpham118 <longp430@gmail.com>
2024-10-05 19:08:27 +0700

    add MenuScene

---
+++ Assets/Scripts/MenuScene/MenuController.cs
@@ -0,0 +1,211 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Net.NetworkInformation;
+
+public class MenuController : MonoBehaviour
+{
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextvalue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 1.0f;
+
+    [Header("Gameplay Settings")]
+    [SerializeField] private TMP_Text controllerSenTextValue = null;
+    [SerializeField] private Slider controllerSenSlider = null;
+    [SerializeField] private int defaultSen = 4;
+    public int mainControllerSen = 4;
+
+    [Header("Toggle Settings")]
+    [SerializeField] private Toggle  invertYToggle = null;
+
+    [Header("Graphics Settings")]
+    [SerializeField] private Slider brightnessSlider = null;
+    [SerializeField] private TMP_Text brightnessTextValue = null;
+    [SerializeField] private float defaultBrightness = 1;
+
+    [Space(10)]
+    [SerializeField] private TMP_Dropdown qualityDropdown;
+    [SerializeField] private Toggle fullScreenToggle;
+
+    private int _qualityLevel;
+    private bool _isFullscreen;
+    private float _brightnessLevel;
+
+    [Header("Confirmation")]
+    [SerializeField] private GameObject comfirmationPrompt = null;
+
+    [Header("Levels To Load")]
+    public string _newGameLevel;
+    private string levelToLoad;
+    [SerializeField] private GameObject noSaveGameDialog = null;
+
+    [Header("Resolution Dropdowns")]
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " +  resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void NewGameDialogYes()
+    {
+        SceneManager.LoadScene(_newGameLevel);
+    }
+
+    public void LoadGameDialogYes()
+    {
+        if (PlayerPrefs.HasKey("SaveLevel"))
+        {
+            levelToLoad = PlayerPrefs.GetString("SavedLevel");
+            SceneManager.LoadScene(levelToLoad);
+        }
+        else
+        {
+            noSaveGameDialog.SetActive(true);
+        }
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextvalue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void SetControllerSen(float sensitivity)
+    {
+        mainControllerSen = Mathf.RoundToInt(sensitivity);
+        controllerSenTextValue.text = sensitivity.ToString("0");
+    }
+
+    public void GameplayApply()
+    {
+        if (invertYToggle.isOn)
+        {
+            PlayerPrefs.SetInt("masterInvertY", 1);
+            //invert Y
+        }
+        else
+        {
+            PlayerPrefs.SetInt("masterInvert", 0);
+            //Not invert 
+        }
+
+        PlayerPrefs.SetFloat("masterSen", mainControllerSen);
+        StartCoroutine (ConfirmationBox());
+    }
+
+    public void SetBrightness(float brightness)
+    {
+        _brightnessLevel = brightness;
+        brightnessTextValue.text = brightness.ToString("0,0");
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        _isFullscreen = isFullScreen;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        _qualityLevel = qualityIndex;
+    }
+
+    public void GraphicsApply()
+    {
+        PlayerPrefs.SetFloat("masterBrightness", _brightnessLevel);
+        //Change your brightness with your post processing or whatever it is
+
+        PlayerPrefs.SetInt("masterQuality", _qualityLevel);
+        QualitySettings.SetQualityLevel(_qualityLevel);
+
+        PlayerPrefs.SetInt("masterFullscreen", (_isFullscreen ? 1 : 0));
+        Screen.fullScreen = _isFullscreen;
+
+        StartCoroutine (ConfirmationBox());
+
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if (MenuType == "Graphics")
+        {
+            //Reset brightness value
+            brightnessSlider.value = defaultBrightness;
+            brightnessTextValue.text = defaultBrightness.ToString("0.0");
+
+            qualityDropdown.value = 1;
+            QualitySettings.SetQualityLevel(1);
+
+            fullScreenToggle.isOn = false;
+            Screen.fullScreen = false;
+
+            Resolution currentResolution = Screen.currentResolution;
+            Screen.SetResolution(currentResolution.width, currentResolution.height, Screen.fullScreen);
+            resolutionDropdown.value = resolutions.Length;
+            GraphicsApply();
+        }
+
+        if (MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextvalue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+
+        if (MenuType == "Gameplay")
+        {
+            controllerSenTextValue.text = defaultSen.ToString("0");
+            controllerSenSlider.value = defaultSen;
+            mainControllerSen = defaultSen;
+            invertYToggle.isOn = false;
+            GameplayApply();
+        }
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        comfirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        comfirmationPrompt.SetActive(false);
+    }
+}
