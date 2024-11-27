a519a5c16f4fe56e26548138f8c098abe734b8c0
HaPhamTrungNghia <104117941+HaPhamTrungNghia@users.noreply.github.com>
2024-11-27 23:50:09 +0700

    add jump

--- Assets/Scripts/PlayerController/CharacterLocomotion.cs
+++ Assets/Scripts/PlayerController/CharacterLocomotion.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class CharacterLocomotion : MonoBehaviour
 {
+    public float jumHeight;
+    public float gravity;
+    public float stepDown;
+    public float airControl;
+    public float jumpDamp;
+    public float groundSpeed;
+
     Animator animator;
+    CharacterController cc;
     Vector2 input;
 
+    Vector3 rootMotion;
+    Vector3 velocity;
+    bool isJumping;
     void Start()
     {
         animator = GetComponent<Animator>();
+        cc = GetComponent<CharacterController>();
     }
 
     void Update()
@@ -19,5 +31,74 @@ public class CharacterLocomotion : MonoBehaviour
 
         animator.SetFloat("InputX", input.x);
         animator.SetFloat("InputY", input.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+
+        }
+    }
+    private void OnAnimatorMove()
+    {
+        rootMotion += animator.deltaPosition;
+    }
+    private void FixedUpdate()
+    {
+        if (isJumping) // is in air state
+        {
+            UpdateInAir();
+        }
+        else // is grounded state
+        {
+            UpdateOnGround();
+        }
+    }
+
+    private void UpdateInAir()
+    {
+        velocity.y -= gravity * Time.fixedDeltaTime;
+        Vector3 displacement = velocity * Time.fixedDeltaTime;
+        displacement += CaculateAirControl();
+        cc.Move(displacement);
+        isJumping = !cc.isGrounded;
+        rootMotion = Vector3.zero;
+        animator.SetBool("isJumping", isJumping);
+    }
+
+    private void UpdateOnGround()
+    {
+        Vector3 StepForwardAmount = rootMotion * groundSpeed;
+        Vector3 StepDownAmount = Vector3.down * stepDown;
+        
+        cc.Move(StepForwardAmount + StepDownAmount);
+        rootMotion = Vector3.zero;
+
+        if (!cc.isGrounded)
+        {
+            SetInAir(0);
+        }
+    }
+
+    Vector3 CaculateAirControl()
+    {
+        return((transform.forward * input.y) + (transform.right * input.x)) * (airControl / 100);
+    }
+
+    void Jump()
+    {
+        if (!isJumping)
+        {
+            float jumpVelocity = Mathf.Sqrt(2 * gravity * jumHeight);
+            SetInAir(jumpVelocity);
+
+        }
+    }
+
+    private void SetInAir(float jumpVelocity)
+    {
+        isJumping = true;
+        velocity = animator.velocity * jumpDamp * groundSpeed;
+        velocity.y = jumpVelocity;
+        animator.SetBool("isJumping", true);
     }
 }
